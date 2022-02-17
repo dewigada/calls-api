481818ada9de067664b17c2c3221a208ba9853c3
dewi.oktaviani <dewi.oktaviani@gudangada.com>
2022-02-17 08:24:53 +0700

    Initial

---
+++ fb-marketing-api.py
@@ -0,0 +1,23 @@
+
+from facebook_business.adobjects.adaccount import AdAccount
+from facebook_business.adobjects.campaign import Campaign
+from facebook_business.api import FacebookAdsApi
+
+access_token = '<ACCESS_TOKEN>'
+app_secret = '<APP_SECRET>'
+app_id = '<APP_ID>'
+id = '<AD_ACCOUNT_ID>'
+FacebookAdsApi.init(access_token=access_token)
+
+fields = [
+  'name',
+  'objective',
+]
+params = {
+  'effective_status': ['ACTIVE','PAUSED'],
+}
+print AdAccount(id).get_campaigns(
+  fields=fields,
+  params=params,
+)
+
